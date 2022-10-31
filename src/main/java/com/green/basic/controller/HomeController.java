bc7a07bded0ce2e0826b901eb05cd4be27f77f06
Jiseokjun <aktmxj4014@naver.com>
2022-10-31 14:48:30 +0900

    dfdf

---
+++ src/main/java/com/green/basic/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.green.basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	// /(root) : http://localhost:8080 
+	@RequestMapping("/")
+	public  String   home() {
+		return  "home";     // /WEB-INF/views/home.jsp
+	}
+	
+	// /List
+	@RequestMapping("/List")
+	public String list() {
+		return "list";
+	}
+}
+
+
+
+
+
+
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<response>
+<header>
+<resultCode>99</resultCode>
+<resultMsg>최대 조회 기간은 오늘 기준으로 3일 전까지입니다.</resultMsg>
+</header>
+</response>
+</body>
+</html>
